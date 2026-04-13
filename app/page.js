3e9fd8935eb38b3aadb853642b53e6770a452e1f
phuhuyhcm <phuhuy2k5ql@gmail.com>
2026-04-13 14:14:04 +0700

    feat: Xay dung BlogCard va fetch data

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
-export default function Home() {
-  return (
-    <div>
-
-    </div>
-  );
+async function getPosts() {
+const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+return res.json();
+}
+export default async function HomePage() {
+const posts = await getPosts();
+// ... render ... 
+return (
+  <div></div>
+)
 }
