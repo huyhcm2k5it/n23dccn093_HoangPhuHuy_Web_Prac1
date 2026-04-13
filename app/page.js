8daec73f5bf87e33e8d08ae098486743eb55b2f3
phuhuyhcm <phuhuy2k5ql@gmail.com>
2026-04-13 14:17:13 +0700

    feat: complete blog listing page with responsive grid

--- app/page.js
+++ app/page.js
@@ -1,11 +1,22 @@
+import Header from '../components/Header';
+import BlogCard from '../components/BlogCard';
+
 async function getPosts() {
 const res = await fetch('https://jsonplaceholder.typicode.com/posts');
 return res.json();
 }
+
 export default async function HomePage() {
 const posts = await getPosts();
-// ... render ... 
+
 return (
-  <div></div>
-)
+  <div>
+    <Header />
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+    {posts.slice(0, 12).map(post => (
+      <BlogCard key={post.id} post={post} />
+    ))}
+    </div>
+  </div>
+);
 }
